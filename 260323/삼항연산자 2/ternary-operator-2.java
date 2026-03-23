340b95b64a58468df4f21f258d5148503d6b4a37
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2026-03-23 17:38:56 +0900

    Title: 삼항연산자 2, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260323/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        String s = a == 1 ? "t" : "f";
+
+        System.out.println(s);
+    }
+}
