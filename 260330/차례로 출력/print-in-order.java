db90de0c67e9af1affede0cff418b141c9782ca4
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2026-03-30 09:37:23 +0900

    Title: 차례로 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/차례로 출력/print-in-order.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 5; i <= 17; ++i) {
+            System.out.print(i + " ");
+        }
+    }
+}
